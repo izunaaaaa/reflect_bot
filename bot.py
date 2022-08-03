2110043a2f735e89ea6e8fc35cb761ed740fc7c1
izunaaaaa <86826214+izunaaaaa@users.noreply.github.com>
2022-08-03 13:31:06 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,8 +11,8 @@ bot = discord.Client()
 
 @tasks.loop(seconds=1)
 async def check_time():
-    if (datetime.now().hour==13 and datetime.now().minute== 00 and datetime.now().second==00):
-        channel = bot.get_channel(509635293175873538)
+    if (datetime.now().hour==13 and datetime.now().minute== 33 and datetime.now().second==00):
+        channel = bot.get_channel(1003532339672002562)
         await channel.send('지금 몇시냐?')
 
     elif (datetime.now().hour==13 and datetime.now().minute==4 and datetime.now().second==00):
